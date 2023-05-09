2aa9a774879037a9e06b8cf07890d6b4ad14e39b
SeyiAkinwale <72584469+SeyiAkinwale@users.noreply.github.com>
2023-05-09 16:50:43 +0000

    max value numbers1

--- .ccls-cache/@home@runner@Warmup/main.cpp
+++ .ccls-cache/@home@runner@Warmup/main.cpp
@@ -7,9 +7,24 @@ using namespace std;
 float maxValue(vector<float> numbers)
 {
   float max = -numeric_limits<float>::infinity();
-  return;
+  for(auto num :numbers)
+    if(num>max) max = num;
+  return 1.0;
 }
 
-int main() {
-  cout <<numeric_limits<float>infinity;
+int main()
+{
+  std::vector<float> numbers1{ 10, 5, 40, 40.3 };
+
+  std::vector<float> numbers2{ -5, -2, -1, -11 };
+
+  std::vector<float> numbers3{ 42 };
+
+  std::vector<float> numbers4{ 1000, 8 };
+
+  std::vector<float> numbers5{ 1000, 8, 9000 };
+
+  std::vector<float> numbers6{ 2, 5, 1, 1, 4 };
+  cout<<endl<<maxValue(numbers1);
+
 }
